3cadd8165d07fcd4fbe365fc2fb5dba0c63a6095
creasyj51 <creasyj51@gmail.com>
2026-04-22 19:23:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,56 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => {
+  res.type("text").send("Serveur AutoDiag OK");
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.post("/signalement", async (req, res) => {
+  try {
+    const immat = req.body.immat || "";
+    const priorite = req.body.priorite || "";
+    const nature = req.body.nature || "";
+    const localisation = req.body.localisation || "";
+    const description = req.body.description || "";
+    const centre = req.body.centre || "";
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    });
+
+    const texte =
+      "Nouveau signalement AutoDiag\n\n" +
+      "Centre : " + centre + "\n" +
+      "Immatriculation : " + immat + "\n" +
+      "Priorité : " + priorite + "\n" +
+      "Nature : " + nature + "\n" +
+      "Localisation : " + localisation + "\n" +
+      "Description : " + description + "\n";
+
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: process.env.EMAIL_TO,
+      subject: "Nouveau signalement AutoDiag - " + immat,
+      text: texte
+    });
+
+    res.status(200).send("OK");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("ERREUR");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Serveur AutoDiag en ecoute sur le port " + port);
+});
